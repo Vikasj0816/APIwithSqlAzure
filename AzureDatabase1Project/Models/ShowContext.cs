b14e92256338695c3aabc6389243c860c26599a9
Vikasj0816 <vikasj0816@gmail.com>
2021-03-29 11:01:11 +0530

    Intializing

---
+++ AzureDatabase1Project/Models/ShowContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureDatabase1Project.Models
+{
+    public class ShowContext:DbContext
+    {
+        public ShowContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Movie>().HasData(new Movie()
+            {
+                Id=1,
+                Name="Robert",
+                Description="Drama",
+                Duration=135
+            });
+        }
+    }
+}
